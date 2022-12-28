08e98c8bdd08be479fb13dc6ad21731f96f230d0
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2022-12-28 17:02:50 +0800

    KMSP-280: create navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,37 @@
+import { Box, AppBar, Toolbar, Typography } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+import PersonIcon from '@mui/icons-material/Person';
+import KamiLogo from '../../public/images/kami-nav-logo.svg';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Box component="header">
+      <AppBar className="bg-kami-white static">
+        <Toolbar>
+          <Box sx={{ flexGrow: 1 }}>
+            <Link href="/dashboard">
+              <Image className="hover:cursor-pointer" src={KamiLogo} alt="Kami logo" />
+            </Link>
+          </Box>
+          <Box component="div" className="flex items-center gap-3">
+            <Typography component="p" className="text-black">
+              Erika Brodnock
+            </Typography>
+            <PersonIcon
+              style={{
+                color: '#666665',
+                backgroundColor: '#F2EFEB',
+                borderRadius: 100,
+                padding: 5,
+                fontSize: '1.875rem',
+              }}
+            />
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'kami-green': '#66C5BF',
+        'kami-white': '#FAF8F6',
+        'kami-blue': '#284F73',
+      },
+    },
   },
   plugins: [],
-}
+};
